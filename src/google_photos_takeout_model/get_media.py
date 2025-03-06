5bd9fbd47d758cc77ef86eae0636a09dd6d207a1
blakeNaccarato <blake.naccarato@gmail.com>
2025-03-06 10:08:54 -0800

    Simplify implementation since photo grabbing is flaky, fix off-by-one error

--- src/google_photos_takeout_model/get_media.py
+++ src/google_photos_takeout_model/get_media.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-import re
 from asyncio import run
 from json import loads
-from nt import environ
 from pathlib import Path
+from re import compile
 from sys import argv
 
 from playwright.async_api import Locator, Page
@@ -12,6 +11,105 @@ from pydantic import BaseModel, Field
 from tqdm import tqdm
 
 from google_photos_takeout_model import dumps, logged_in
+
+GPHOTOS_ALBUM = argv[1] if len(argv) > 1 else ""
+ALBUM = Path("album.json")
+
+
+async def main():
+    async with logged_in() as pg:
+        await get_all_media(pg)
+
+
+async def get_all_media(pg: Page, album: str = GPHOTOS_ALBUM):
+    if not album:
+        raise ValueError("Album link required.")
+    await pg.goto(album)
+    if ALBUM.exists():
+        alb = Album(**loads(ALBUM.read_text(encoding="utf-8")))
+    else:
+        alb = Album(
+            title=(await pg.title()).removesuffix(" - Google Photos"),
+            item=pg.url,
+        )
+        ALBUM.write_text(
+            encoding="utf-8",
+            data=dumps_album(alb),
+        )
+    await nav_first(pg)
+    pg_main = pg.get_by_role("main")
+    await pg_main.get_by_label("Open info").click()
+    album_item_count = await get_item_count(pg)
+    current_media_item = len(alb.media_items)
+    for _ in tqdm(range(1, current_media_item)):
+        await nav_next(pg_main)
+    for _ in tqdm(range(current_media_item, album_item_count)):
+        await nav_next(pg_main)
+        await get_media_item(pg, alb)
+
+
+async def get_media_item(pg: Page, album: Album):
+    pg_main = pg.get_by_role("main")
+    info = pg_main.locator("c-wiz").filter(
+        has=pg.get_by_role("button", name="Close info")
+    )
+    all_details = (
+        await info.locator("div").filter(has=pg.locator("> dt > svg")).all_inner_texts()
+    )
+    download = (
+        "" if "(0 B)" in "".join(all_details) else await get_download_url(pg, pg_main)
+    ).removesuffix("authuser=0")
+    map = pg.get_by_role("link", name="Map")
+    position = map.locator("[position]").first.get_attribute("position")
+    people = info.get_by_role("link", name="Photo of ")
+    albums = info.get_by_role("link", name=compile(r"\d+\sitems"))
+    album.media_items.append(
+        MediaItem(
+            item=pg.url,
+            download=download,
+            people=await people.all_inner_texts(),
+            albums=await albums.all_inner_texts(),
+            details=all_details,
+            position=(await position or "") if await map.count() else "",
+        )
+    )
+    ALBUM.write_text(encoding="utf-8", data=dumps_album(album))
+
+
+def dumps_album(album: Album) -> str:
+    return dumps(
+        album.model_dump(),
+        indent=2,
+        ensure_ascii=False,
+    )
+
+
+async def get_item_count(pg: Page) -> int:
+    loc_desc = "meta[property='og:description'][content*='items added to shared album']"
+    return (
+        int(desc.split()[0])
+        if (desc := await pg.locator(loc_desc).get_attribute("content"))
+        else 0
+    )
+
+
+async def get_download_url(pg: Page, loc: Locator) -> str:
+    async with pg.expect_download() as downloader:
+        await loc.press("Shift+D")
+    download = await downloader.value
+    await download.cancel()
+    download_url = download.url
+    return download_url
+
+
+async def nav_first(pg: Page):
+    await pg.get_by_role("link", name=compile(r"^(?!Back|Goog).*$")).first.click()
+
+
+async def nav_next(loc: Locator):
+    prev_url = loc.page.url
+    await loc.press("ArrowRight")
+    await loc.page.wait_for_url(lambda url: url != prev_url)
 
 
 class MediaItem(BaseModel):
@@ -27,112 +125,6 @@ class Album(BaseModel):
     title: str = ""
     item: str = ""
     media_items: list[MediaItem] = Field(default_factory=list)
-
-
-ALBUM = Path("album.json")
-
-
-async def main():
-    async with logged_in() as pg:
-        await get_all_media(pg)
-
-
-async def get_all_media(pg: Page):
-    album_url = argv[1] if len(argv) > 1 else environ["GPHOTOS_ALBUM"]
-    m = pg.get_by_role("main")
-    loc_desc = "meta[property='og:description'][content*='items added to shared album']"
-    await pg.goto(album_url)
-    if ALBUM.exists():
-        album = Album(**loads(ALBUM.read_text(encoding="utf-8")))
-    else:
-        album = Album(
-            title=(await pg.title()).removesuffix(" - Google Photos"),
-            item=pg.url,
-        )
-        ALBUM.write_text(
-            encoding="utf-8",
-            data=dumps(
-                album.model_dump(),
-                indent=2,
-                ensure_ascii=False,
-            ),
-        )
-    await pg.get_by_role("link", name=re.compile(r"^(?!Back|Goog).*$")).first.click()
-    await m.get_by_label("Open info").click()
-    current_media_item = len(album.media_items)
-    media_item_count = (
-        int(desc.split()[0])
-        if (desc := await pg.locator(loc_desc).get_attribute("content"))
-        else 0
-    )
-    for _ in tqdm(range(current_media_item)):
-        await nav_next(m)
-    for _ in tqdm(range(current_media_item, media_item_count)):
-        await nav_next(m)
-        await get_media_item(pg, album)
-
-
-async def nav_next(m: Locator):
-    prev_url = m.page.url
-    await m.press("ArrowRight")
-    await m.page.wait_for_url(lambda url: url != prev_url)
-
-
-async def get_media_item(pg: Page, album: Album):
-    m = pg.get_by_role("main")
-    panes = m.locator("c-wiz")
-    # TODO: Detects stale video elements for a few items afterwards.
-    # TODO: Use combination of `is_enabled`, `is_visible`, or something to filter down
-    # TODO: Not a huge deal, since `Shift+D` points to downloads for pictures as well
-    video = panes.nth(1).get_by_title("YouTube Google Photos Video Player")
-    info = panes.filter(has=pg.get_by_role("button", name="Close info"))
-    position = pg.get_by_role("link", name="Map")
-    details = (
-        await info.locator("div").filter(has=pg.locator("> dt > svg")).all_inner_texts()
-    )
-    if "(0 B)" in "".join(details):
-        download_url = ""
-    elif await video.count():
-        async with pg.expect_download() as downloader:
-            await m.press("Shift+D")
-        download = await downloader.value
-        await download.cancel()
-        download_url = download.url
-    else:
-        download_url = await get_css_attr(
-            m.filter(has=pg.get_by_role("img", name="Photo - ")).last, "src"
-        )
-    info.get_by_role("link", name="Photo of ")
-    info.get_by_label("File size: ")
-    album.media_items.append(
-        MediaItem(
-            item=pg.url,
-            download=re.sub(r"\?authuser=0$", "", download_url),
-            people=[
-                await iloc.inner_text()
-                for iloc in await info.get_by_role("link", name="Photo of ").all()
-            ],
-            albums=[
-                await iloc.inner_text()
-                for iloc in await info.get_by_role(
-                    "link", name=re.compile(r"\d+\sitems")
-                ).all()
-            ],
-            details=details,
-            position=(
-                await get_css_attr(position, "position")
-                if await position.count()
-                else ""
-            ),
-        )
-    )
-    ALBUM.write_text(
-        encoding="utf-8", data=dumps(album.model_dump(), indent=2, ensure_ascii=False)
-    )
-
-
-async def get_css_attr(locator: Locator, attr: str = "") -> str:
-    return await locator.locator(f"[{attr}]").first.get_attribute(attr) or ""
 
 
 if __name__ == "__main__":
